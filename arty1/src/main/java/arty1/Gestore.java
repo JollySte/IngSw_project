22e61bdcf4a1f43073a1a03d39ead780161e3ee2
SteArz <stefano.arzani@studenti.unimi.it>
2021-11-04 22:57:51 +0100

    Gestore: aggiunti metodi vuoti

--- arty1/src/main/java/arty1/Gestore.java
+++ arty1/src/main/java/arty1/Gestore.java
@@ -1,5 +1,16 @@
 package arty1;
 
 public class Gestore {
-
+	
+	
+	//costruttori
+	public Gestore() {}
+	
+	
+	//metodi
+	initSession(){}  //prepara una sessione di voto
+	setVoteMode(){}  //imposta la modalità di voto
+	setWinMode(){}  //imposta la modlità di scelta del vincitore
+	endSession(){}  //termina la sessione di voto
+	voteCount(){}  //conteggio voti
 }
