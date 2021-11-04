69be80f60c33a5fc5f420b545428e9a2e8b9f524
SteArz <stefano.arzani@studenti.unimi.it>
2021-11-04 23:22:14 +0100

    aggiunta classe SessioneVoto

---
+++ arty1/src/main/java/arty1/SessioneVoto.java
@@ -0,0 +1,15 @@
+package arty1;
+
+public class SessioneVoto {
+
+	//costruttori
+	public SessioneVoto() {}
+	
+	//metodi
+	checkUsr(){}    //verifica che tipo di utente sta interagendo
+	addCand(){}     //aggiunge un candidato alla lista
+	rmCand(){}      //rimuove un dandidato dalla lista
+	getList(){}		//mostra la lista dei candidati
+	voteCount(){}  //conteggio voti
+	
+}
