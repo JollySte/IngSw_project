7229a4d5b09fc95e0211c38bed5c88b72769cb5b
SteArz <stefano.arzani@studenti.unimi.it>
2021-11-04 22:58:07 +0100

    Elettore: aggiunti metodi vuoti

--- arty1/src/main/java/arty1/Elettore.java
+++ arty1/src/main/java/arty1/Elettore.java
@@ -1,5 +1,16 @@
 package arty1;
 
 public class Elettore {
+	
+	//costruttori
+	public Elettore() {}
+	
+	
+	//metodi
+	checkId(){} //controllo documento identità
+	checkVoted(){} //controllo che l'utente non abbia già votato
+	vote(){}		//sceglie un candidato da una lista
+	
+	
 
 }
